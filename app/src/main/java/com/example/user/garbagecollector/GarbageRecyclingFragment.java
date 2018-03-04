dc81069d3404d572b87646ee04b24ceda183c289
DansingWithWolfs <dimikson4@gmail.com>
2018-03-04 13:16:52 +0300

    Added IntroActivity and Fragment

---
+++ app/src/main/java/com/example/user/garbagecollector/GarbageRecyclingFragment.java
@@ -0,0 +1,22 @@
+package com.example.user.garbagecollector;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by User on 04.03.2018.
+ */
+
+public class GarbageRecyclingFragment extends Fragment {
+
+    public void onAttach(Activity activity){
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
